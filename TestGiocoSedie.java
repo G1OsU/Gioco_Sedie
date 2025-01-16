4f0bbb0ad5b2faf5e323464e2e66d092fbb1353e
G1OsU <giosue.laurenzi@risikamente.it>
2025-01-16 12:57:11 +0100

    Upload Scrittore

--- TestGiocoSedie.java
+++ TestGiocoSedie.java
@@ -13,12 +13,14 @@ import java.util.logging.Logger;
 public class TestGiocoSedie {
     private final static int NUMSEDIE = 15;
     private static Logger logger = Logger.getLogger("GiocoSedie.TestGiocoSedie");
-    
+
+
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         Posto sedie[] = new Posto[NUMSEDIE];
+
 
 	for (int k = 0; k < sedie.length; k++)
 		sedie[k] = new Posto();
@@ -26,14 +28,15 @@ public class TestGiocoSedie {
 	Display display = new Display(sedie);
 	//System.out.println("Sto facendo partire il Display.");
         logger.info("Sto facendo partire il Display.\n");
-	display.start();
+        display.start();
 
 	Partecipante array[] = new Partecipante[NUMSEDIE+1];
 	for (int i = 0; i < NUMSEDIE + 1; i++) {
 		array[i] = new Partecipante(sedie);
                 //System.out.println("Sto facendo partire il thread n." + array[i].getId());
                 logger.info("Sto facendo partire il thread id: " + array[i].getId()+" name: "+array[i].getName()+"\n");
-                array[i].start();
+
+        array[i].start();
                 }
 	}
     }
